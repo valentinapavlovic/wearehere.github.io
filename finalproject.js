b40729095f2665ea2e641e89d045adb990d4da61
jmagee02 <41705602+jmagee02@users.noreply.github.com>
2018-08-08 10:06:19 -0700

    Update finalproject.js
    outside url redirecting.

--- finalproject.js
+++ finalproject.js
@@ -1,1 +1,7 @@
-js
+<button id="myButton" class="float-left submit-button" >Home</button>
+
+<script type="text/javascript">
+    document.getElementById("myButton").onclick = function () {
+        location.href = "www.(our site url).com";
+    };
+</script>
